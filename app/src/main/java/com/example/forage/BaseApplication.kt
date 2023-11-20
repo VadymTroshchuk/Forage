b4af5231aab792324bd9d2c6d8af530100c36ef9
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-20 20:08:24 +0200

    implement dDatabase class

--- app/src/main/java/com/example/forage/BaseApplication.kt
+++ app/src/main/java/com/example/forage/BaseApplication.kt
@@ -1,14 +1,11 @@
-
 package com.example.forage
 
 import android.app.Application
 import com.example.forage.data.ForageDatabase
 
-/**
- * An application class that inherits from [Application], allows for the creation of a singleton
- * instance of the [ForageDatabase]
- */
-class BaseApplication : Application() {
 
-    // TODO: provide a ForageDatabase value by lazy here
+class BaseApplication : Application() {
+    val database: ForageDatabase by lazy {
+        ForageDatabase.getDatabase(this)
+    }
 }
